23f65b833043f452c3acfdcc845efe6c99123b7a
elkana911 <ellkana@gmail.com>
2019-11-12 10:53:26 +0700

    Update Hello.java

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -1,8 +1,11 @@
 package com.example.hello;
 
+// The native Toast API
+import android.widget.Toast;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
-
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,7 +21,30 @@ public class Hello extends CordovaPlugin {
             String message = args.getString(0);
             this.coolMethod(message, callbackContext);
             return true;
+        }else
+        if (action.equals("show")) {
+            String message;
+            String duration;
+            try {
+                JSONObject options = args.getJSONObject(0);
+                message = options.getString("message");
+                duration = options.getString("duration");
+            } catch (JSONException e) {
+                callbackContext.error("Error encountered: " + e.getMessage());
+                return false;
+            }
+            // Create the toast
+            Toast toast = Toast.makeText(cordova.getActivity(), message,
+                DURATION_LONG.equals(duration) ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT);
+            // Display toast
+            toast.show();
+            // Send a positive result to the callbackContext
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
+            callbackContext.sendPluginResult(pluginResult);
+            return true;
         }
+        
+        callbackContext.error("\"" + action + "\" is not a recognized action.");
         return false;
     }
 
