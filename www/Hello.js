1e1ee5162bbadb92374a79d5f59aa05b482e7c3b
elkana911 <ellkana@gmail.com>
2019-11-12 13:18:58 +0700

    Update Hello.js

--- www/Hello.js
+++ www/Hello.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'Hello', 'coolMethod', [arg0]);
 };
+
+exports.toast = function (arg0, success, error) {
+    exec(success, error, 'Hello', 'show', [arg0]);
+};
