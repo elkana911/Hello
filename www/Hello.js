27994e1d43b1a61c4fdfbf0af2f951592b45ece3
elkana911 <ellkana@gmail.com>
2019-11-12 15:03:31 +0700

    Update Hello.js

--- www/Hello.js
+++ www/Hello.js
@@ -1,5 +1,25 @@
 var exec = require('cordova/exec');
 
+function Hello() {}    //empty constructor
+
+Hello.prototype.show = function (message, duration, successCallback, errorCallback){
+    // create anonym object to send to native
+    var obj = {};
+    obj.message = message;
+    obj.duration = duration;
+    cordova.exec(successCallback, errorCallback, 'Hello', 'show', [obj]);
+}
+
+Hello.install = function() {
+    if (!window.plugins) {
+        window.plugins = {};
+    }
+    window.plugins.hello = new Hello();
+    return window.plugins.hello;
+}
+cordova.addConstructor(Hello.install);
+
+/*
 exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'Hello', 'coolMethod', [arg0]);
 };
@@ -11,3 +31,4 @@ exports.toast = function (arg0, success, error) {
     
     exec(success, error, 'Hello', 'show', [obj]);
 };
+*/
